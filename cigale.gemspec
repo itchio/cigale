c68f79ca9bd1e70f25c0dcab967d2cf6e3938bd2
Amos Wenger <fasterthanlime@gmail.com>
2015-11-29 23:42:04 +0100

    Basic matrix project

--- cigale.gemspec
+++ cigale.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_runtime_dependency "slop", "~> 4.2.1"
+  spec.add_runtime_dependency "builder", "~> 3.2.2"
 end
--- lib/cigale.rb
+++ lib/cigale.rb
@@ -1,10 +1,17 @@
 require "cigale/version"
+require "logger"
 require "slop"
 require "yaml"
+require "fileutils"
+require "builder"
 
 module Cigale
   class CLI
     def initialize
+      @logger = Logger.new($stdout).tap do |log|
+        log.progname = 'cigale'
+      end
+
       opts = Slop.parse ARGV do |o|
         o.banner = 'Usage: cigale [options] [spec_file.yml]'
 
@@ -20,9 +27,48 @@ module Cigale
         exit 1
       end
 
-      puts "Input = #{input}, output = #{opts[:output]}"
-      f = YAML.load_file(input)
-      puts f.first.keys.first
+      @logger.info "Parsing #{input}" 
+      entries = YAML.load_file(input)
+
+      output = opts[:output]
+      @logger.info "Creating directory #{output}" 
+      FileUtils.mkdir_p output
+
+      for entry in entries
+        case entry.keys.first
+        when 'job'
+          root = entry.values.first
+
+          xml = Builder::XmlMarkup.new(:indent => 2)
+          xml.instruct! :xml, :version => "1.0", :encoding => "utf-8"
+          xml.tag! 'matrix-project' do
+            xml.executionStrategy :class => 'hudson.matrix.DefaultMatrixExecutionStrategyImpl' do
+              xml.runSequentially false
+            end
+            xml.combinationFilter
+            xml.axes
+            xml.actions
+            xml.description "<!-- Managed by Jenkins Job Builder -->"
+            xml.keepDependencies false
+            xml.blockBuildWhenDownstreamBuilding false
+            xml.blockBuildWhenUpstreamBuilding false
+            xml.concurrentBuild false
+            xml.canRoam true
+            xml.properties
+            xml.scm :class => "hudson.scm.NullSCM"
+            xml.builders
+            xml.publishers
+            xml.buildWrappers
+          end
+
+          job_path = File.join(output, root["name"])
+          File.open(job_path, 'w') do |f|
+            f.write(xml.target!)
+          end
+        else
+          raise "Unknown top-level type: #{}"
+        end
+      end
     end
   end
 end
