010bd95679abae0b5114d7b8dce578e836e81717
Amos Wenger <fasterthanlime@gmail.com>
2015-12-01 22:35:37 +0100

    hg: browser support, revision, clean, credentials, etc.

--- lib/cigale/scm/hg.rb
+++ lib/cigale/scm/hg.rb
@@ -2,10 +2,35 @@
 module Cigale::SCM::Hg
   def translate_hg_scm (xml, sdef)
     xml.source sdef["url"]
+    if cid = sdef["credentials-id"]
+      xml.credentialsId cid
+    end
     xml.revisionType "BRANCH"
-    xml.revision "default"
-    xml.clean false
-    xml.modules
-    xml.disableChangeLog false
+    xml.revision sdef["revision"] || "default"
+    if sub = sdef["subdir"]
+      xml.subdir sub
+    end
+    xml.clean sdef["clean"] || false
+    if sdef["modules"]
+      xml.modules sdef["modules"].join " "
+    else
+      xml.modules
+    end
+    xml.disableChangeLog sdef["disable-changelog"] || false
+
+    if browser = sdef["browser"]
+      bclass = hg_browser_classes[browser] or raise "Unknown hg browser type #{browser}"
+      xml.browser :class => bclass do
+        if val = sdef["browser-url"]
+          xml.url val
+        end
+      end
+    end
+  end
+
+  def hg_browser_classes
+    @hg_browser_classes ||= {
+      "hgweb" => "hudson.plugins.mercurial.browser.HgWeb",
+    }
   end
 end
