62864a95b5f90b503dce57f8f7bde13d774227ad
Amos Wenger <fasterthanlime@gmail.com>
2015-12-02 17:46:15 +0100

    managed batch

--- lib/cigale/builder/managed-script.rb
+++ lib/cigale/builder/managed-script.rb
@@ -4,12 +4,19 @@ module Cigale::Builder
     sclass = case bdef["type"]
     when "script"
       "org.jenkinsci.plugins.managedscripts.ScriptBuildStep"
+    when "batch"
+      "org.jenkinsci.plugins.managedscripts.WinBatchBuildStep"
     else
       raise "Unknown managed script type: #{bdef["type"]}"
     end
 
     xml.tag! sclass do
-      xml.buildStepId bdef["script-id"]
+      case bdef["type"]
+      when "script"
+        xml.buildStepId bdef["script-id"]
+      when "batch"
+        xml.command bdef["script-id"]
+      end
       xml.buildStepArgs do
         for arg in bdef["args"]
           xml.string arg
