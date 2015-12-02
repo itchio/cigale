b4a097b924510a52e17ff380a85fa5ef52abadaa
Amos Wenger <fasterthanlime@gmail.com>
2015-12-02 15:53:26 +0100

    more system groovy

--- lib/cigale/builder/system-groovy.rb
+++ lib/cigale/builder/system-groovy.rb
@@ -1,12 +1,16 @@
 
 module Cigale::Builder
   def translate_system_groovy_builder (xml, bdef)
-    if file = bdef["file"]
+    if cmd = bdef["command"]
+      xml.scriptSource :class => "hudson.plugins.groovy.StringScriptSource" do
+        xml.command cmd
+      end
+    elsif file = bdef["file"]
       xml.scriptSource :class => "hudson.plugins.groovy.FileScriptSource" do
         xml.scriptFile file
       end
     end
-    xml.bindings
-    xml.classpath
+    xml.bindings bdef["bindings"]
+    xml.classpath bdef["class-path"]
   end
 end
