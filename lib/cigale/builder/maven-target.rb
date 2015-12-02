32cb7f21863508250338f78d1780565d6b854bec
Amos Wenger <fasterthanlime@gmail.com>
2015-12-02 16:09:10 +0100

    maven target

--- lib/cigale/builder/maven-target.rb
+++ lib/cigale/builder/maven-target.rb
@@ -2,16 +2,29 @@
 module Cigale::Builder
   def translate_maven_target_builder (xml, bdef)
     xml.targets bdef["goals"]
-    xml.properties
+    if props = bdef["properties"]
+      xml.properties props.join(",")
+    else
+      xml.properties
+    end
     xml.mavenName bdef["maven-version"]
-    xml.usePrivateRepository false
 
-    xml.settings :class => "org.jenkinsci.plugins.configfiles.maven.job.MvnSettingsProvider" do
-      xml.settingsConfigId bdef["settings"]
+    privrepo = bdef["private-repository"]
+    xml.usePrivateRepository !!privrepo
+    if settings = bdef["settings"]
+      xml.settings :class => "org.jenkinsci.plugins.configfiles.maven.job.MvnSettingsProvider" do
+        xml.settingsConfigId settings
+      end
+    else
+      xml.settings :class => "jenkins.mvn.DefaultSettingsProvider"
     end
 
-    xml.globalSettings :class => "org.jenkinsci.plugins.configfiles.maven.job.MvnGlobalSettingsProvider" do
-      xml.settingsConfigId bdef["global-settings"]
+    if gsettings = bdef["global-settings"]
+      xml.globalSettings :class => "org.jenkinsci.plugins.configfiles.maven.job.MvnGlobalSettingsProvider" do
+        xml.settingsConfigId gsettings
+      end
+    else
+      xml.globalSettings :class => "jenkins.mvn.DefaultGlobalSettingsProvider"
     end
   end
 end
