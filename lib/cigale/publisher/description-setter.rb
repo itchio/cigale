833797a5abce876eaf8f52593cf755fea92a3f99
Amos Wenger <fasterthanlime@gmail.com>
2015-12-04 00:14:35 +0100

    description setter publisher

--- lib/cigale/publisher/description-setter.rb
+++ lib/cigale/publisher/description-setter.rb
@@ -1,5 +1,9 @@
 module Cigale::Publisher
   def translate_description_setter_publisher (xml, pdef)
-    xml.fixme
+    xml.regexp pdef["regexp"]
+    xml.regexpForFailed pdef["regexp-for-failed"]
+    xml.description pdef["description"]
+    xml.descriptionForFailed pdef["description-for-failed"]
+    xml.setForMatrix pdef["set-for-matrix"]
   end
 end
