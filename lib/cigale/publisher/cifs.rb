d7d4d619c9bbe1ed6db1d4fa1792b5c29f2e3c4a
Amos Wenger <fasterthanlime@gmail.com>
2015-12-03 21:13:34 +0100

    cifs publisher

--- lib/cigale/publisher/cifs.rb
+++ lib/cigale/publisher/cifs.rb
@@ -1,5 +1,32 @@
 module Cigale::Publisher
   def translate_cifs_publisher (xml, pdef)
-    xml.fixme
-  end
+    xml.consolePrefix "CIFS: "
+    xml.delegate do
+      xml.publishers do
+        xml.tag! "jenkins.plugins.publish__over__cifs.CifsPublisher" do
+          xml.configName pdef["site"]
+          xml.verbose true
+          xml.transfers do
+            xml.tag! "jenkins.plugins.publish__over__cifs.CifsTransfer" do
+              xml.remoteDirectory pdef["target"]
+              xml.sourceFiles pdef["source"]
+              xml.excludes pdef["excludes"]
+              xml.removePrefix pdef["remove-prefix"]
+              xml.remoteDirectorySDF false
+              xml.flatten pdef["flatten"]
+              xml.cleanRemote false
+            end # CifsTransfer
+          end #transfers
+
+          xml.useWorkspaceInPromotion false
+          xml.usePromotionTimestamp false
+        end # CifsPublisher
+      end # publishers
+
+      xml.continueOnError false
+      xml.failOnError false
+      xml.alwaysPublishFromMaster false
+      xml.hostConfigurationAccess :class => "jenkins.plugins.publish_over_cifs.CifsPublisherPlugin", :reference => "../.."
+    end # delegate
+  end # translate
 end
