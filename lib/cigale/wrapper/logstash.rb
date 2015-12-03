8bb235ba01fa3c5ddaa88481a683aa917cbf2284
Amos Wenger <fasterthanlime@gmail.com>
2015-12-03 15:26:53 +0100

    logstash redis block

--- lib/cigale/wrapper/logstash.rb
+++ lib/cigale/wrapper/logstash.rb
@@ -3,14 +3,16 @@ module Cigale::Wrapper
   def translate_logstash_wrapper (xml, wdef)
     xml.tag! "jenkins.plugins.logstash.LogstashBuildWrapper", :plugin => "logstash@0.8.0" do
       xml.useRedis wdef["use-redis"]
-      if redis = wdef["redis"]
+
+      if wdef["use-redis"] == true
+        redis = (wdef["redis"] || {})
         xml.redis do
-          xml.host redis["host"]
-          xml.port redis["port"]
-          xml.numb redis["database-number"]
+          xml.host redis["host"] || "localhost"
+          xml.port redis["port"] || 6379
+          xml.numb redis["database-number"] || 0
           xml.pass redis["database-password"]
-          xml.dataType redis["data-type"]
-          xml.key redis["key"]
+          xml.dataType redis["data-type"] || "list"
+          xml.key redis["key"] || "logstash"
         end
       end
     end
