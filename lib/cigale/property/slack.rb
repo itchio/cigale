b7025d34ddb91dfdae045eb29384309d74c8272d
Amos Wenger <fasterthanlime@gmail.com>
2015-12-14 02:43:57 +0100

    *sigh* there's no test for the wicked

--- lib/cigale/property/slack.rb
+++ lib/cigale/property/slack.rb
@@ -8,18 +8,18 @@ module Cigale::Property
     notify = toh pdef["notify"]
 
     xml.startNotification boolp(notify["start"], false)
-    xml.startNotification boolp(notify["success"], false)
-    xml.startNotification boolp(notify["aborted"], false)
-    xml.startNotification boolp(notify["not-built"], false)
-    xml.startNotification boolp(notify["unstable"], false)
-    xml.startNotification boolp(notify["failure"], false)
-    xml.startNotification boolp(notify["back-to-normal"], false)
-    xml.startNotification boolp(notify["repeated-failure"], false)
+    xml.notifySuccess boolp(notify["success"], false)
+    xml.notifyAborted boolp(notify["aborted"], false)
+    xml.notifyNotBuilt boolp(notify["not-built"], false)
+    xml.notifyUnstable boolp(notify["unstable"], false)
+    xml.notifyFailure boolp(notify["failure"], false)
+    xml.notifyBackToNormal boolp(notify["back-to-normal"], false)
+    xml.notifyRepeatedFailures boolp(notify["repeated-failure"], false)
 
-    xml.startNotification boolp(pdef["include-test-summary"], false)
-    xml.startNotification boolp(notify["show-commit-list"], false)
+    xml.includeTestSummary boolp(pdef["include-test-summary"], false)
+    xml.showCommitList boolp(notify["show-commit-list"], false)
 
-    xml.startNotification !!pdef["custom-message"]
+    xml.includeCustomMessage !!pdef["custom-message"]
     xml.customMessage pdef["custom-message"]
   end
 end
